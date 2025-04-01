8d8dae6b9ee4c7bb9b0013fbe6e75e975be75022
Ivyyy294 <48586445+Ivyyy294@users.noreply.github.com>
2025-04-01 11:21:36 +0200

    🎉 assignment1scene

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "core/Application.h"
 #include "scenes/TestScene.h"
+#include "scenes/AssignmentScene1.h"
 
 int main(int argc, char** argv) {
     // Add newly created scenes here so that they show up in the menu.
     std::vector<Scene*> scenes = {
         new TestScene(),
+		  new AssignmentScene1()
     };
     Application app(scenes);
     return app.Run();
---
+++ src/scenes/AssignmentScene1.cpp
@@ -0,0 +1,16 @@
+#include "AssignmentScene1.h"
+
+#include "imgui.h"
+
+void AssignmentScene1::OnEnable() {}
+
+void AssignmentScene1::OnDisable() {}
+
+void AssignmentScene1::Update(float deltaTime) {}
+
+void AssignmentScene1::Draw() {}
+
+void AssignmentScene1::DrawGUI() {
+    ImGui::Begin("Inspector");
+    ImGui::End();
+}
